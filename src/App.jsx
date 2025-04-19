03bce434ae61034733e996998b930c4193826bac
debitkatuwal96-websitedeveloper <katuwalroshan790@gmail.com>
2025-04-18 23:35:45 -0500

    ROlex_clone

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+function App() {
+
+
+  return (
+    <>
+      <div className="absolute top-0 left-0 w-full z-10">
+        
+      </div>
+    </>
+  )
+}
+
+export default App
